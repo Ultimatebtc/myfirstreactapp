a85d157d3122d7c4cc634ba671e57c508a05943b
Destiny <destinymutairu03@gmail.com>
2025-06-26 13:20:29 +0100

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-        hello
+    <div className='flex items-center justify-center h-dvh'>
+        <h1 className='text-3xl font-bold text-blue-700'>This is the main branch</h1>
     </div>
   )
 }
