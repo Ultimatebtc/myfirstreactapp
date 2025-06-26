d98625842d4aebba7ebbdab5f92ad8a99fe43ad4
Destiny <destinymutairu03@gmail.com>
2025-06-26 12:47:19 +0100

    bg color

---
+++ app/courses/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        courses
+        <h1 className='bg-red-600 text-white'>hello Courses</h1>
+        <div></div>
+        <main></main>
+        <article></article>
+        <blockquote></blockquote>
+    </>
+  )
+}
+
+export default page
