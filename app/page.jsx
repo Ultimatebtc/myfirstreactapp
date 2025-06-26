819bead389f9bb8697c1b423265cb82e3fc874d1
Destiny <destinymutairu03@gmail.com>
2025-06-26 13:44:23 +0100

    added page

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex items-center justify-center h-dvh'>
-        <h1 className='text-3xl font-bold text-blue-700'>This is the main branch</h1>
+        <h1 className='text-3xl font-bold text-red-700'>This is the test branch</h1>
+        <h2>This is the main page</h2>
     </div>
   )
 }
